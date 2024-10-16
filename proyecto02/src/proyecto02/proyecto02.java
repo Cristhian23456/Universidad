945b8a572b219a9155d3bccd34607a5ef876eb32
Cristhian23456 <cristhianalbertoquizhpe@gmail.com>
2024-10-16 10:16:16 -0500

    tercer commit, trabajando con git y GitHubClasroom

---
+++ proyecto02/src/proyecto02/proyecto02.java
@@ -0,0 +1,8 @@
+package proyecto02;
+
+public class proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además se usar GitHubClassroom");
+    }
+}
+
